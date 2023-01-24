bfdbc935bd02cdb7071061ecf4a39a1d22707cc3
NaragudeNitin <naragudenitin@gmail.com>
2023-01-24 23:02:25 +0530

    Listening in different places and ways

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:my_shop_app/providers/products.dart';
+import 'package:provider/provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   // const ProductDetailScreen({super.key, required this.title});
-
   // final String title;
   static const routeName = '/product-detail';
-
   const ProductDetailScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context)?.settings.arguments as String;
+    final loadedProduct =
+        Provider.of<Products>(context, listen: false).findById(productId);
     return Scaffold(
       appBar: AppBar(
-        title: const Text("title"),
+        title: Text(loadedProduct.title),
       ),
     );
   }
+  /**
+   * -when you dont want to change your data whenever 
+   *   item is added then we can use listen argument as 'false' in of() after provider
+   */
 }
