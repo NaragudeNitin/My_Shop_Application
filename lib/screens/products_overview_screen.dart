f64a34efcacbf4088ffa64ebc65bd9707b35da5b
NaragudeNitin <naragudenitin@gmail.com>
2023-01-24 14:01:01 +0530

    created pozo class Products

---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ProductsOverViewScreen extends StatelessWidget {
+  const ProductsOverViewScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //created products class to add products
+    );
+  }
+}
