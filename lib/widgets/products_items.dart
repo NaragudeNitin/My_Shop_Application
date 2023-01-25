22d2bf99ddb25c30dc706fa67e4a3c5021881f10
NaragudeNitin <naragudenitin@gmail.com>
2023-01-25 10:02:50 +0530

    used consumer instead of provider

--- lib/widgets/products_items.dart
+++ lib/widgets/products_items.dart
@@ -6,23 +6,29 @@ import 'package:provider/provider.dart';
 class ProductItem extends StatelessWidget {
   const ProductItem({super.key});
 
-
-
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(context, listen: false);
+    print("product builds first time only");
     return ClipRRect(
       //ClipRRect forces the widget to take certain shape
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
+
+          //********Consumer listner**********/
+          leading: Consumer<Product>(
+            builder: (context, product, child) => IconButton(
               color: Theme.of(context).colorScheme.secondary,
               onPressed: () {
-                product.toggleFavoriteStatus(); 
+                product.toggleFavoriteStatus();
               },
-              icon: Icon(product.isFavorite ? Icons.favorite : Icons.favorite_border)),
+              icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border),
+            ),
+            child: const Text("this widget never changes!!"),
+          ),
           title: Text(
             product.title,
             textAlign: TextAlign.center,
@@ -34,17 +40,15 @@ class ProductItem extends StatelessWidget {
         ),
         child: GestureDetector(
           onTap: () {
-           Navigator.of(context).pushNamed(
-            ProductDetailScreen.routeName, 
-            arguments: product.id);
+            Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
+                arguments: product.id);
           },
           child: Image.network(
             product.imageUrl,
             fit: BoxFit.cover,
           ),
         ),
-        
-      ), 
+      ),
     );
   }
 }
