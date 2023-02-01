af6bfbc417ecc7e6e04e5d820728f99744ea612f
NaragudeNitin <naragudenitin@gmail.com>
2023-02-01 12:02:32 +0530

    added

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:my_shop_app/providers/product.dart';
+import 'package:http/http.dart' as http; 
 
 class Products with ChangeNotifier {
   final List<Product> _items = [
@@ -62,8 +65,41 @@ class Products with ChangeNotifier {
   //   _showFavoritesOnly = false;
   //   notifyListeners();
   // }
-  void addProduct() {
-    // _items.add();
+  void addProduct(Product product) {
+    const url = 'https://myshopapp-becc5-default-rtdb.firebaseio.com/products.json';
+    http.post((Uri.parse(url)), body: jsonEncode({
+      'title' : product.title,
+      'description' :product.description,
+      'imageUrl' : product.imageUrl,
+      'price' : product.price,
+      'isFavorite' : product.isFavorite
+    }),).then((response) {
+      final newProduct = Product(
+    id: json.decode(response.body)['name'], 
+    title: product.title, 
+    description: product.description, 
+    price: product.price,
+     imageUrl: product.imageUrl
+     );
+     _items.add(newProduct);
+    //  _items.insert(0, newProduct); //at the start of the list
+    notifyListeners();
+    });
+   
+  }
+
+  void updateProduct(String id, Product newProduct){
+    final prodIndex = _items.indexWhere((prod) => prod.id == id);
+    if (prodIndex >= 0) {
+    _items[prodIndex] = newProduct;
+    notifyListeners();
+    }else{
+      print("....");
+    }
+  }
+
+  void deleteProduct(String id){
+    _items.removeWhere((prod) => prod.id == id);
     notifyListeners();
   }
 }
